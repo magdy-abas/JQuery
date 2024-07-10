6793bd0b818c692ac7515413576a517f86c1026a
Magdy Abas <132621536+magdy-abas@users.noreply.github.com>
2024-07-10 15:04:14 +0300

    handle slider error

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,4 @@
-//left menu
-
+// Side navigation menu open/close
 $(".openNav").click(function () {
   $("#leftMenu").animate({ width: "250px" }, 50);
   $("#home-content").animate({ marginLeft: "250px" }, 50);
@@ -10,27 +9,30 @@ $(".closebtn").click(function () {
   $("#home-content").animate({ marginLeft: "0px" }, 50);
 });
 
-//scroll menu
-$("#leftMenu a").click(function () {
+// Smooth scrolling
+$("#leftMenu a").click(function (e) {
+  e.preventDefault();
+
   var sectionId = $(this).attr("href");
 
-  var positionOfSection = $(sectionId).offset().top;
-
-  $("html , body").animate({ scrollTop: positionOfSection });
+  // Only animate if the href is not just "#"
+  if (sectionId !== "#" && sectionId !== "") {
+    var positionOfSection = $(sectionId).offset().top;
+    $("html, body").animate({ scrollTop: positionOfSection }, 1000); // Duration in milliseconds
+  }
 });
 
-//Accordion
-
+// Accordion
 $("#sliderDown .toggle").click(function () {
   $(".inner").not($(this).next()).slideUp(500);
   $(this).next().slideToggle(500);
 });
 
-// Countdown
-let date = new Date("jul 25, 2024 21:00:00").getTime();
+// Countdown timer
+let targetDate = new Date("Jul 25, 2024 21:00:00").getTime();
 setInterval(function () {
   let now = new Date().getTime();
-  let distance = date - now;
+  let distance = targetDate - now;
 
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -43,8 +45,7 @@ setInterval(function () {
   $(".seconds").text(seconds);
 }, 1000);
 
-/*textarea*/
-
+// Character counter
 $("textarea").keyup(function () {
   let max = 100;
   let length = $(this).val().length;
